835023e415a613fd009c5e27d4737710f01972f6
John Erling Blad <jeblad@gmail.com>
2018-11-13 05:05:42 +0100

    Updated Hooks

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,8 +2,6 @@
 
 namespace LangCodeOverride;
 
-use \MediaWiki\MediaWikiServices;
-
 /**
  * Hook handlers for the LangCodeOverride extension
  *
@@ -15,10 +13,7 @@ class Hooks {
 	 * Setup for the extension
 	 */
 	public static function onExtensionSetup() {
-		global $wgDebugComments;
-
-		// turn on comments while in development
-		//$wgDebugComments = true;
+		// empty
 	}
 
 	/**
@@ -28,27 +23,26 @@ class Hooks {
 		$files[] = __DIR__ . '/../tests/phpunit/';
 	}
 
-
 	/**
 	 * Override the language link
 	 * This tries to mimic the inner actions of SkinTemplate::getLanguages()
-	 * 
+	 *
 	 * Some consequences of mimicing existing code
-	 * 
+	 *
 	 * An formal parameter is passed on, which is never used.
 	 * @SuppressWarnings(PHPMD.UnusedFormalParameters)
-	 * 
+	 *
 	 * A very long variable name is used, it could be dropped, chose to keep it.
 	 * @SuppressWarnings(PHPMD.LongVariable)
-	 * 
+	 *
 	 * The original code uses else clauses, kept to make code similar.
 	 * @SuppressWarnings(PHPMD.ElseExpression)
-	 * 
+	 *
 	 * @param array &$languageLink containing data about the link
 	 * @param string $overrideLangCode the language code to use
 	 * @param Title $languageLinkTitle object for the external language link
 	 * @param Title $title object for the page the link belongs to
-	 * @param OutputPage $outputPage for the page the link belongs to
+	 * @param OutputPage $output for the page the link belongs to
 	 * @param Language $language for testing purposes
 	 * @param LanguageCode $languageCode for testing purposes
 	 */
@@ -57,11 +51,11 @@ class Hooks {
 		$overrideLangCode,
 		$languageLinkTitle,
 		$title,
-		$outputPage,
+		$output,
 		$language = \Language::class,
 		$languageCode = \LanguageCode::class
 	) {
-		$skin = $outputPage->getContext()->getSkin();
+		$skin = $output->getContext()->getSkin();
 		$userLang = $skin->getLanguage();
 
 		$languageLinkText = $languageLinkTitle->getText();
@@ -137,9 +131,9 @@ class Hooks {
 
 	/**
 	 * Get group for the given database id
-	 * 
+	 *
 	 * @SuppressWarnings(PHPMD.StaticAccess)
-	 * 
+	 *
 	 * @param string $dbname the identifier for the database
 	 * @param \MediaWiki\MediaWikiServices $services the provider
 	 * @return string|null the group for the database
@@ -169,7 +163,7 @@ class Hooks {
 	 * @param array &$languageLink containing data about the link
 	 * @param Title $languageLinkTitle object for the external language link
 	 * @param Title $title object for the page the link belongs to
-	 * @param OutputPage $outputPage for the page the link belongs to
+	 * @param OutputPage $output for the page the link belongs to
 	 * @return bool
 	 */
 	public static function onSkinTemplateGetLanguageLink(
@@ -191,34 +185,40 @@ class Hooks {
 		}
 
 		if ( !array_key_exists( $group, $wgLCOverrideCodes ) ) {
-			wfDebugLog( 'LangCodeOverride', "Could not find a '$group' key in override structure." );
+			wfDebugLog( 'LangCodeOverride',
+				"Could not find a '$group' key in override structure." );
 			return true;
 		}
 
 		if ( !array_key_exists( 'lang', $languageLink ) ) {
-			wfDebugLog( 'LangCodeOverride', "Could not find a 'lang' key in link structure." );
+			wfDebugLog( 'LangCodeOverride',
+				"Could not find a 'lang' key in link structure." );
 			return true;
 		}
 
 		$langCode = $languageLink['lang'];
 		if ( !$langCode ) {
-			wfDebugLog( 'LangCodeOverride', "Could not find a value for 'lang' key in link structure." );
+			wfDebugLog( 'LangCodeOverride',
+				"Could not find a value for 'lang' key in link structure." );
 			return true;
 		}
 
 		$overrides = $wgLCOverrideCodes[$group];
 		if ( !$overrides ) {
-			wfDebugLog( 'LangCodeOverride', "Could not find an array for '$group' key in override structure." );
+			wfDebugLog( 'LangCodeOverride',
+				"Could not find an array for '$group' key in override structure." );
 			return true;
 		}
 
 		if ( !array_key_exists( $langCode, $overrides ) ) {
-			wfDebugLog( 'LangCodeOverride', "Could not find a '$langCode' key in override structure." );
+			wfDebugLog( 'LangCodeOverride',
+				"Could not find a '$langCode' key in override structure." );
 			return true;
 		}
 
 		$overrideCode = $overrides[$langCode];
-		wfDebugLog( 'LangCodeOverride', "Overrides language code: $langCode → $overrideCode" );
+		wfDebugLog( 'LangCodeOverride',
+			"Overrides language code: $langCode → $overrideCode" );
 
 		self::overrideLanguageLink(
 			$languageLink,
---
+++ tests/phpunit/includes/HooksTest.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace LangCodeOverride\Tests;
+
+use \LangCodeOverride\Hooks;
+
+/**
+ * @group LangCodeOverride
+ *
+ * @covers \LangCodeOverride\Hooks
+ */
+class HooksTest extends \MediaWikiTestCase {
+
+	/**
+	 * Create a new output page
+	 *
+	 * Not sure if all of this is needed. It is a copy of the private method from
+	 * OutputPageTest::newInstance().\Composer\Autoload\ClassLoader
+	 *
+	 */
+	private function newOutputPage( $config = [], WebRequest $request = null, $options = [] ) {
+		$context = new \RequestContext();
+
+		$context->setConfig( new \MultiConfig( [
+			new \HashConfig( $config + [
+				'AppleTouchIcon' => false,
+				'DisableLangConversion' => true,
+				'EnableCanonicalServerLink' => false,
+				'Favicon' => false,
+				'Feed' => false,
+				'LanguageCode' => false,
+				'ReferrerPolicy' => false,
+				'RightsPage' => false,
+				'RightsUrl' => false,
+				'UniversalEditButton' => false,
+			] ),
+			$context->getConfig()
+		] ) );
+
+		if ( !in_array( 'notitle', (array)$options ) ) {
+			$context->setTitle( \Title::newFromText( 'My test page' ) );
+		}
+
+		if ( $request ) {
+			$context->setRequest( $request );
+		}
+
+		return new \OutputPage( $context );
+	}
+
+	public function provideOverrideLanguageLink() {
+		return [
+			[ 'nb', 'no:foo', null, null ],
+			[ 'nb', 'nb:foo', null, null ],
+			[ 'ping', 'nb:foo', false, false ],
+			[ 'hbs', 'hs:foo', null, null ],
+			[ 'nbs', 'hs:foo', null, null ],
+			[ 'ping', 'hs:foo', false, false ],
+			[ 'en-simple', 'simple:foo', null, null ],
+			[ 'en-simple', 'simple:foo', null, null ],
+			[ 'ping', 'simple:foo', false, false ]
+		];
+	}
+
+	/**
+	 * @dataProvider provideOverrideLanguageLink
+	 *
+	 * @SuppressWarnings(PHPMD.StaticAccess)
+	 *
+	 * @param array &$languageLink containing data about the link
+	 * @param string $overrideLangCode the language code to use
+	 * @param Title $languageLinkTitle object for the external language link
+	 * @param Title $title object for the page the link belongs to
+	 * @param OutputPage $outputPage for the page the link belongs to
+	 */
+	public function testOverrideLanguageLink( $overrideCode, $link, $text = null, $title = null ) {
+		$linkedTitle = \Title::newFromText( $link );
+
+		$output = $this->newOutputPage();
+		$output->setTitle( \Title::newFromText( 'Lang code override' ) );
+		$this->assertSame( 'Lang code override', $output->getTitle()->getPrefixedText() );
+
+		$languageLink = [
+			'href' => $linkedTitle->getFullURL(),
+			'text' => 'none',
+			'title' => $linkedTitle,
+			'class' => 'interlanguage-link interwiki-' . $linkedTitle->getInterwiki(),
+			'link-class' => 'interlanguage-link-target',
+			'lang' => $linkedTitle->getInterwiki(),
+			'hreflang' => $linkedTitle->getInterwiki()
+		];
+
+		Hooks::overrideLanguageLink(
+			$languageLink,
+			$overrideCode,
+			$linkedTitle,
+			$output->getTitle(),
+			$output
+		);
+
+		// this should still exist
+		$this->assertNotNull( $languageLink );
+
+		// the href should not be changed
+		$this->assertArrayHasKey( 'href', $languageLink );
+		$this->assertEquals( $linkedTitle->getFullURL(), $languageLink['href'] );
+
+		// the link class should not be changed
+		$this->assertArrayHasKey( 'link-class', $languageLink );
+		$this->assertEquals( 'interlanguage-link-target', $languageLink['link-class'] );
+
+		// the language code should be the override code
+		$this->assertArrayHasKey( 'lang', $languageLink );
+		$this->assertEquals( $overrideCode, $languageLink['lang'] );
+
+		// the href language code should be the override code
+		$this->assertArrayHasKey( 'hreflang', $languageLink );
+		$this->assertEquals( $overrideCode, $languageLink['hreflang'] );
+
+		// the class should use the override code
+		$this->assertArrayHasKey( 'class', $languageLink );
+		$this->assertEquals( "interlanguage-link interwiki-$overrideCode", $languageLink['class'] );
+
+		// the text should be the language name for the override code
+		$this->assertArrayHasKey( 'text', $languageLink );
+		if ( $text === null ) {
+			$skin = $output->getContext()->getSkin();
+			$text = $skin->formatLanguageName( \Language::fetchLanguageName( $overrideCode ) );
+			$text = '/' . preg_quote( $text ) . '/i';
+		} elseif ( $text === true ) {
+			// this should pick up a previously set message
+			$text = '/' . '⧼' . 'interlanguage-link-' . $overrideCode . '\b' . '/i';
+		} elseif ( $text === false ) {
+			$text = '/' . preg_quote( $link ) . '/i';
+		} elseif ( gettype( $text ) === 'string' ) {
+			// nop
+		} else {
+			$this->fail( 'Should not be here…' );
+		}
+		$this->assertRegExp( $text, $languageLink['text'] );
+
+		// the title should be a compostite with a site name
+		$this->assertArrayHasKey( 'title', $languageLink );
+		if ( $title === null ) {
+			$skin = $output->getContext()->getSkin();
+			$langName = $skin->formatLanguageName( \Language::fetchLanguageName( $overrideCode ) );
+			$title = '/' . preg_quote( $langName ) . '/i';
+		} elseif ( $title === true ) {
+			// this should pick up a previously set message
+			$title = '/' . '⧼' . 'interlanguage-link-' . $overrideCode . '\b' . '/i';
+		} elseif ( $title === false ) {
+			$title = '/:' . preg_quote( $link ) . '/i';
+		} elseif ( gettype( $title ) === 'string' ) {
+			$title = '/' . preg_quote( $title ) . '/i';
+		} else {
+			$this->fail( 'Should not be here…' );
+		}
+		$this->assertRegExp( $title, $languageLink['title'] );
+	}
+}
